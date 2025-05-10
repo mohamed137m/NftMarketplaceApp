c3b411ce390f981ff8f407ff31778d9449a7e1d4
mohamed137m <mohamedmodi776@gmail.com>
2025-05-10 21:42:55 +0300

    bottomNavigationBar

--- lib/Presentation/home/home_screen.dart
+++ lib/Presentation/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:nft_marketplace/core/resources/Strings_title.dart';
 import 'package:nft_marketplace/core/resources/colors_value.dart';
@@ -13,7 +15,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double WidthScreen = MediaQuery.of(context).size.width;
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         centerTitle: true,
@@ -32,7 +36,108 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+
+      bottomNavigationBar: SizedBox(
+        height: 122.52592468261719,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              bottom: 0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(45.05),
+                  topRight: Radius.circular(45.05),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 72.07, sigmaY: 72.07),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: WidthScreen,
+                      height: 90.09259033203125,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white.withValues(alpha: 0.1),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(45.05),
+                          topRight: Radius.circular(45.05),
+                        ),
+                        color: Colors.white.withValues(alpha: 0.1),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.home,
+                              color: ColorsValue.kWhiteColor,
+                              size: 40,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.stacked_bar_chart_rounded,
+                              color: ColorsValue.kWhiteColor,
+                              size: 40,
+                            ),
+                          ),
+                          SizedBox(width: 70.27222442626953),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.search_rounded,
+                              color: ColorsValue.kWhiteColor,
+                              size: 40,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.person,
+                              color: ColorsValue.kWhiteColor,
+                              size: 40,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 70.27222442626953,
+                  height: 70.27222442626953,
+                  decoration: ShapeDecoration(
+                    shape: StarBorder.polygon(sides: 6, pointRounding: .3),
+                    color: ColorsValue.kWhiteColor.withValues(alpha: 0.6),
+                  ),
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      color: ColorsValue.kWhiteColor,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: ColorsValue.kPrimaryColor,
+
       body: ListView(
         children: [
           const SizedBox(height: 30),
@@ -76,6 +181,7 @@ class HomeScreen extends StatelessWidget {
               },
             ),
           ),
+          SizedBox(height: 9),
         ],
       ),
     );
